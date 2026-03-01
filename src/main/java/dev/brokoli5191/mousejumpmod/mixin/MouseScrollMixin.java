4dfc9751a154642bfad1a4986b0a69b3104c1e87
Brokoli5191 <119419930+Brokoli5191@users.noreply.github.com>
2026-03-01 16:49:19 +0100

    Revert mixin to Yarn mapping names: Mouse, onMouseScroll

--- src/main/java/dev/brokoli5191/mousejumpmod/mixin/MouseScrollMixin.java
+++ src/main/java/dev/brokoli5191/mousejumpmod/mixin/MouseScrollMixin.java
@@ -1,18 +1,18 @@
 package dev.brokoli5191.mousejumpmod.mixin;
 
 import dev.brokoli5191.mousejumpmod.MouseJumpModClient;
-import net.minecraft.client.MouseHandler;
+import net.minecraft.client.Mouse;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MouseHandler.class)
+@Mixin(Mouse.class)
 public class MouseScrollMixin {
 
-    @Inject(method = "onScroll", at = @At("HEAD"))
-    private void onScroll(long window, double xOffset, double yOffset, CallbackInfo ci) {
-        if (yOffset > 0) MouseJumpModClient.scrolledUp = true;
-        else if (yOffset < 0) MouseJumpModClient.scrolledDown = true;
+    @Inject(method = "onMouseScroll", at = @At("HEAD"))
+    private void onMouseScroll(long window, double horizontal, double vertical, CallbackInfo ci) {
+        if (vertical > 0) MouseJumpModClient.scrolledUp = true;
+        else if (vertical < 0) MouseJumpModClient.scrolledDown = true;
     }
 }
